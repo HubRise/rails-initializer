d94e9d57f7b167b0e8f069dc769708879a4d4b73
antmonnier <monniera@gmail.com>
2024-07-25 11:19:25 +0200

    LoggerWithOptions

---
+++ lib/act_fluent_logger_rails/logger_with_options.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module ActFluentLoggerRails
+  class LoggerWithOptions
+    def self.new(**kwargs)
+      Logger.new(kwargs)
+    end
+  end
+
+  module FluentLoggerExtension
+    def initialize(options, level, log_tags)
+      super(options, level, log_tags)
+
+      # Rebuild Fluent Logger with custom options
+      logger_opts = default_logger_options(options).merge(
+        use_nonblock: true
+      )
+      @fluent_logger = ::Fluent::Logger::FluentLogger.new(nil, logger_opts)
+    end
+
+    private
+
+    # Return same options as the original class
+    def default_logger_options(options)
+      logger_opts = {
+        host: options[:host],
+        port: options[:port],
+        nanosecond_precision: options[:nanosecond_precision],
+      }
+      logger_opts[:tls_options] = options[:tls_options] unless options[:tls_options].nil?
+      logger_opts
+    end
+  end
+end
+
+ActFluentLoggerRails::FluentLogger.prepend(ActFluentLoggerRails::FluentLoggerExtension)
